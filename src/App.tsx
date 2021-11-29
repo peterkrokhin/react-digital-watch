9113afee0d5af6332fbd0158a22873978e1dc816
peterkrokhin <p.krokhin@gmail.com>
2021-11-30 00:19:39 +0500

    added number to digits mapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ export const App = () => {
   return (
     <div className="App">
       Hello
-      <Digit />
+      <Digit number={0}/>
+      <Digit number={1}/>
+      <Digit number={2}/>
+      <Digit number={3}/>
+      <Digit number={4}/>
+      <Digit number={5}/>
+      <Digit number={6}/>
+      <Digit number={7}/>
+      <Digit number={8}/>
+      <Digit number={9}/>
     </div>
   );
 }
--- src/components/clockface/digit/Digit.tsx
+++ src/components/clockface/digit/Digit.tsx
@@ -1,28 +1,50 @@
 import styles from './Digit.module.css';
 
+const numberToDigitMapper: Map<number, boolean[]> = new Map([
+    [0, [true, true, true, false, true, true, true]],
+    [1, [false, false, true, false, false, true, false]],
+    [2, [true, false, true, true, true, false, true]],
+    [3, [true, false, true, true, false, true, true]],
+    [4, [false, true, true, true, false, true, false]],
+    [5, [true, true, false, true, false, true, true]],
+    [6, [true, true, false, true, true, true, true]],
+    [7, [true, false, true, false, false, true, false]],
+    [8, [true, true, true, true, true, true, true]],
+    [9, [true, true, true, true, false, true, true]],
+]);
 
-export const Digit = () => {
-  return (
-    <div>
-      <div className={styles.digit}>
-            <div className={styles.horizontal_area}>
-                <div className={styles.horizontal_line_on}></div>
-            </div>
-            <div className={styles.vertical_area}>
-                <div className={styles.vertical_line_on}></div>
-                <div className={styles.vertical_line_on}></div>
-            </div>
-            <div className={styles.horizontal_area}>
-                <div className={styles.horizontal_line_on}></div>
-            </div>
-            <div className={styles.vertical_area}>
-                <div className={styles.vertical_line_off}></div>
-                <div className={styles.vertical_line_off}></div>
-            </div>
-            <div className={styles.horizontal_area}>
-                <div className={styles.horizontal_line_off}></div>
+const defaultDigitElements = [true, true, true, true, true, true, true];
+
+interface DigitProps {
+    number: number;
+}
+
+export const Digit = ({ number }: DigitProps) => {
+    const digitsElements = numberToDigitMapper.get(number) || defaultDigitElements;
+    const lineClasses = digitsElements.map(d => 
+        d ? styles.line_on : styles.line_off);
+    console.log(lineClasses);
+    return (
+        <div>
+        <div className={styles.digit}>
+                <div className={styles.horizontal_area}>
+                    <div className={`${styles.horizontal_line} ${lineClasses[0]}`}></div>
+                </div>
+                <div className={styles.vertical_area}>
+                    <div className={`${styles.vertical_line} ${lineClasses[1]}`}></div>
+                    <div className={`${styles.vertical_line} ${lineClasses[2]}`}></div>
+                </div>
+                <div className={styles.horizontal_area}>
+                    <div className={`${styles.horizontal_line} ${lineClasses[3]}`}></div>
+                </div>
+                <div className={styles.vertical_area}>
+                    <div className={`${styles.vertical_line} ${lineClasses[4]}`}></div>
+                    <div className={`${styles.vertical_line} ${lineClasses[5]}`}></div>
+                </div>
+                <div className={styles.horizontal_area}>
+                    <div className={`${styles.horizontal_line} ${lineClasses[6]}`}></div>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
