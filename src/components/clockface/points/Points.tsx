7d8d5cac73f0e37cc535197263da64029e48dad8
peterkrokhin <p.krokhin@gmail.com>
2021-11-30 16:37:51 +0500

    added points props

--- src/components/clockface/points/Points.tsx
+++ src/components/clockface/points/Points.tsx
@@ -1,10 +1,15 @@
 import styles from './Points.module.css';
 
-export const Points = () => {
+interface PointsProps {
+    pointsOn: boolean;
+}
+
+export const Points = ({ pointsOn }: PointsProps) => {
+    const pointOnClass = pointsOn ? styles.on : styles.off;
     return (
         <div className={styles.points}>
-            <div className={`${styles.point} ${styles.on}`}></div>
-            <div className={`${styles.point} ${styles.off}`}></div>
+            <div className={`${styles.point} ${pointOnClass}`}></div>
+            <div className={`${styles.point} ${pointOnClass}`}></div>
         </div>
     );
 };
