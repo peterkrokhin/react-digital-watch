f61874a8b123f0e82ea1a0c4fc7fc94bd073497e
peterkrokhin <p.krokhin@gmail.com>
2021-11-30 11:39:51 +0500

    cleaned

--- src/components/clockface/digit/Digit.tsx
+++ src/components/clockface/digit/Digit.tsx
@@ -24,25 +24,23 @@ export const Digit = ({ number }: DigitProps) => {
         d ? styles.line_on : styles.line_off);
 
     return (
-        <div>
         <div className={styles.digit}>
-                <div className={styles.horizontal_area}>
-                    <div className={`${styles.horizontal_line} ${lineClasses[0]}`}></div>
-                </div>
-                <div className={styles.vertical_area}>
-                    <div className={`${styles.vertical_line} ${lineClasses[1]}`}></div>
-                    <div className={`${styles.vertical_line} ${lineClasses[2]}`}></div>
-                </div>
-                <div className={styles.horizontal_area}>
-                    <div className={`${styles.horizontal_line} ${lineClasses[3]}`}></div>
-                </div>
-                <div className={styles.vertical_area}>
-                    <div className={`${styles.vertical_line} ${lineClasses[4]}`}></div>
-                    <div className={`${styles.vertical_line} ${lineClasses[5]}`}></div>
-                </div>
-                <div className={styles.horizontal_area}>
-                    <div className={`${styles.horizontal_line} ${lineClasses[6]}`}></div>
-                </div>
+            <div className={styles.horizontal_area}>
+                <div className={`${styles.horizontal_line} ${lineClasses[0]}`}></div>
+            </div>
+            <div className={styles.vertical_area}>
+                <div className={`${styles.vertical_line} ${lineClasses[1]}`}></div>
+                <div className={`${styles.vertical_line} ${lineClasses[2]}`}></div>
+            </div>
+            <div className={styles.horizontal_area}>
+                <div className={`${styles.horizontal_line} ${lineClasses[3]}`}></div>
+            </div>
+            <div className={styles.vertical_area}>
+                <div className={`${styles.vertical_line} ${lineClasses[4]}`}></div>
+                <div className={`${styles.vertical_line} ${lineClasses[5]}`}></div>
+            </div>
+            <div className={styles.horizontal_area}>
+                <div className={`${styles.horizontal_line} ${lineClasses[6]}`}></div>
             </div>
         </div>
     );
