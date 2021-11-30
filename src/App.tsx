b139df939e9e20a23a4207cf219319395041e9e1
peterkrokhin <p.krokhin@gmail.com>
2021-11-30 16:38:28 +0500

    added clockface props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Clockface } from './components/clockface/Clockface';
 
+const clockTime = {
+    hoursFistDigit: 1,
+    hoursSecondDigit: 2,
+    minutesFirstDigit: 0,
+    minutesSecondDigit: 0,
+    pointsIndicatorOn: true
+}
+
 export const App = () => {
     return (
         <div className="App">
-            <Clockface />
+            <Clockface clockTime={clockTime}/>
         </div>
     );
 }
--- src/components/clockface/Clockface.tsx
+++ src/components/clockface/Clockface.tsx
@@ -2,14 +2,24 @@ import { Digit } from "./digit/Digit";
 import { Points } from "./points/Points";
 import styles from './Clockface.module.css';
 
-export const Clockface = () => {
+interface ClockfaceProps {
+    clockTime: {
+        hoursFistDigit: number;
+        hoursSecondDigit: number;
+        minutesFirstDigit: number;
+        minutesSecondDigit: number;
+        pointsIndicatorOn: boolean
+    }
+}
+
+export const Clockface = ({ clockTime }: ClockfaceProps) => {
     return(
         <div id={styles.clockface}>
-            <Digit number={0}/>
-            <Digit number={1}/>
-            <Points />
-            <Digit number={2}/>
-            <Digit number={3}/>
+            <Digit number={clockTime.hoursFistDigit}/>
+            <Digit number={clockTime.hoursSecondDigit}/>
+            <Points pointsOn={clockTime.pointsIndicatorOn}/>
+            <Digit number={clockTime.minutesFirstDigit}/>
+            <Digit number={clockTime.minutesSecondDigit}/>
         </div>
     );
 };
