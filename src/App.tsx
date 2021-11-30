6ffb71b174a3c6a26847f2bd636c9e0dd3e97b62
peterkrokhin <p.krokhin@gmail.com>
2021-11-30 18:07:42 +0500

    added clock logic prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Clockface } from './components/clockface/Clockface';
 
-const clockTime = {
-    hoursFistDigit: 1,
-    hoursSecondDigit: 2,
-    minutesFirstDigit: 0,
-    minutesSecondDigit: 0,
-    pointsIndicatorOn: true
-}
+const getFirstAndSecondDigits = (number: number) => {
+    const firstDigit = Math.trunc(number / 10);
+    const secondDigit = number - firstDigit * 10;
+    return ({
+        firstDigit: firstDigit,
+        secondDigit: secondDigit
+    });
+};
+
+const getClockTime = () => {
+    const nowDt = new Date();
+    const hours = nowDt.getHours();
+    const minutes = nowDt.getMinutes();
+    const seconds = nowDt.getSeconds();
+
+    return ({
+        hoursFistDigit: getFirstAndSecondDigits(hours).firstDigit,
+        hoursSecondDigit: getFirstAndSecondDigits(hours).secondDigit,
+        minutesFirstDigit: getFirstAndSecondDigits(minutes).firstDigit,
+        minutesSecondDigit: getFirstAndSecondDigits(minutes).secondDigit,
+        pointsIndicatorOn: seconds % 2 ? true: false
+    });
+};
 
 export const App = () => {
+    const [clockTime, setClockTime] = useState(getClockTime());
+    
+    useEffect(() => {
+        setInterval(() => {
+            setClockTime(getClockTime());         
+        }, 1000);
+    });
+
     return (
         <div className="App">
             <Clockface clockTime={clockTime}/>
         </div>
     );
-}
+};
